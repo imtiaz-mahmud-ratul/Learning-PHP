705222451e6aca32e4fafc387d0b87ce86fb0f98
Imtiaz Mahmud <imtu.business@gmail.com>
2025-09-05 21:52:21 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+echo "Hello, Imtiaz! <br>";
+echo"Its my first PHP file";
+//This is a comment
+
+/*
+This is a multi-line comment
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>Order Pizza</button>
+</body>
+</html>
